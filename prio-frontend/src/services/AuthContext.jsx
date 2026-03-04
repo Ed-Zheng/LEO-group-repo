82649696e1920f9b2c9a202f54d34a015a0c384b
Edison Zheng <sketchyazx@gmail.com>
2026-03-04 16:14:43 -0500

    Fixed typo

--- prio-frontend/src/services/AuthContext.jsx
+++ prio-frontend/src/services/AuthContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { auth } from './firebase';
-import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'Firebase/auth';
+import { auth } from './Firebase';
+import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
 
 const AuthContext = createContext();
 
